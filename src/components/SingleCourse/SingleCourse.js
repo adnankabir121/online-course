7c047463188fad9fb5ff46ba9f6fb6e4823ab40c
adnankabir121 <adnankabir618@gmail.com>
2020-08-28 14:30:48 +0600

    updated

--- src/components/SingleCourse/SingleCourse.js
+++ src/components/SingleCourse/SingleCourse.js
@@ -6,7 +6,7 @@ const SingleCourse = (props) => {
             <div className="single-course p-5 my-4">
                 <h5 className="mb-3">Course Name : {props.course.name}</h5>
                 <h5 className="mb-4">Price : {props.course.price}</h5>
-                <button onClick={()=> props.handleAddCourse(props.course)} className="btn btn-primary">Add Course</button>
+                <button onClick={()=> props.handleAddCourse(props.course)} className="btn btn-primary">Enroll Now</button>
             </div>
 
         </div>
